1c4846d1f9c1afdf4872eba927a83a445df12d31
parallels <siempre112>
2018-03-27 20:24:38 +0200

    Programa 1: cambio variables

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -3,9 +3,14 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
+#Cabeceras, con la petición GET indicamos que navegador somos
+
 conn = http.client.HTTPSConnection("api.fda.gov")
+#creamos una conexión con openfda al url de la api
 conn.request("GET", "/drug/label.json", None, headers)
+#pedimos el recurso
 r1 = conn.getresponse()
+#la respuesta que te da openfda
 print(r1.status, r1.reason)
 codigo_json = r1.read().decode("utf-8")
 conn.close()
@@ -18,9 +23,11 @@ conn.close()
 
 
 esquema_json = json.loads(codigo_json)
+#la libreria json nos carga el archivo "en crudo"
 medicamento_info=esquema_json['results'][0]
 
-print ('ID: ',medicamento_info['id'])
-print ('Proposito: ',medicamento_info['purpose'][0])
 
-print ('Fabricante: ',medicamento_info['openfda']['manufacturer_name'][0])
+print ('ID: ', medicamento_info['id'])
+print ('Purpose: ',medicamento_info['purpose'][0])
+
+print ('Manufacture: ',medicamento_info['openfda']['manufacturer_name'][0])
