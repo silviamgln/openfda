6ed128058a042c003f9eddedff5b3c1a3c012738
parallels <siempre112>
2018-04-03 19:47:32 +0200

    Comentarios programa1

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -5,18 +5,16 @@ headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json?limit=10", None, headers)
-#Limit indica que el API nos tiene que devolver 10 medicamentos
-#?limit=10&color=blue"
+#utilizamos limit para que nos devuelva 10 medicamentos
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 codigo_json = r1.read().decode("utf-8")
 conn.close()
 
 esquema_json = json.loads(codigo_json)
-for i in range (len (esquema_json['results']))
-    #longitud de la lista de resultados
-    medicine_info=esquema_json['results'][i]
-    #cada resultado es un diccionario
+#recorremos cada elemento de la lista de los valores de la clave 'results'
+for i in range(len (esquema_json['results'])):
+    medicamento_info=esquema_json['results'][i]
+    #imprimimos el id de cada medicamento
+    print ('ID: ',medicamento_info['id'])
 
-    print ('ID: ',medicine_info['id'])
-#cada resultado es un diccionario
