ff416f5ff670bc8bd0bb0924d8d39231ccbc5cc1
parallels <siempre112>
2018-04-03 20:10:44 +0200

    Comentarios programa3

--- openfda-2/programa3.py
+++ openfda-2/programa3.py
@@ -3,22 +3,31 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", '/drug/label.json?limit=100&search=substance_name:"ASPIRIN"', None, headers)
-#la petición va cambiando porque cambia el num de cosas que quieres pedir
-#Dame 100 relacionados con la aspirina y sáltate los 0 primeros (el skip)
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-codigo_json = r1.read().decode("utf-8")
-conn.close()
+skip_num=0
+while True:
+    conn = http.client.HTTPSConnection("api.fda.gov")
+    conn.request("GET", '/drug/label.json?limit=100&skip='+str(skip_num)+'&search=substance_name:"ASPIRIN"', None, headers)
+    #Nos devuelve 100 mediacamentosrelacionados con la aspirina
+    #y salta los 0 primeros (el skip)
+    #Al volverlo a ejecutar salta los 100 primeros
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    codigo_json = r1.read().decode("utf-8")
+    conn.close()
 
-esquema_json = json.loads(codigo_json)
+    esquema_json = json.loads(codigo_json)
 
-for i in range (len (esquema_json['results'])):
-    medicine_info=esquema_json['results'][i]
-    print ('ID: ',medicine_info['id'])
-    #recorremos todos los medicamentos imprimiendo el id
+    for i in range (len (esquema_json['results'])):
+        medicine_info=esquema_json['results'][i]
+        #recorremos todos los medicamentos imprimiendo el id
+        print ('ID: ',medicine_info['id'])
 
-    if (medicine_info['openfda']):
-        print('Manufacturer: ', medicine_info['openfda']['manufacturer_name'][0])
+        #si el medicamento tiene manufacturer lo imprimimos también
+        if (medicine_info['openfda']):
+            print('Manufacturer: ', medicine_info['openfda']['manufacturer_name'][0])
+
+    #Si se cumple esta condición el bucle while se para
+    if (len(esquema_json['results'])<100):
+        break
+    skip_num=skip_num+100
 
